dfdf6e65d556faadaebb5bf3aa313da2a5b28e10
puthands0me <puthands0me@gmail.com>
2015-03-04 18:38:43 +0100

    Nowa wersja v1

--- Moviement/src/com/example/moviement/MainActivity.java
+++ Moviement/src/com/example/moviement/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //zmiana pliku
+        
+        //kawala pozdro 600 chyba dziala
     }
 
 
