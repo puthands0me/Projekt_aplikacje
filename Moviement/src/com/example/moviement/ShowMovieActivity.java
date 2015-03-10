9f78551a727ea410d0b12a3ea5ca702a054165fe
TheBestPL <kawala.artek@gmail.com>
2015-03-10 09:04:01 +0100

    Dodane WannaSee

--- Moviement/src/com/example/moviement/ShowMovieActivity.java
+++ Moviement/src/com/example/moviement/ShowMovieActivity.java
@@ -3,12 +3,14 @@ package com.example.moviement;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
 
 public class ShowMovieActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getWindow().requestFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_show_movie);
 		//ja eirdosadlsadsajdjcxzcjxzhjsadadaraotafsaddsacxz
 		
