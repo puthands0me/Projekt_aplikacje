e4bc68c0326f8bf6cf669beedffa34ebf2db36d0
Wojciech <Wojciech@wojtek-pc>
2015-03-17 08:56:09 +0100

    bez peirdol

--- Moviement/src/com/example/moviement/MainActivity.java
+++ Moviement/src/com/example/moviement/MainActivity.java
@@ -2,18 +2,43 @@ package com.example.moviement;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Point;
+import android.view.Display;
 import android.view.Menu;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
+	LinearLayout lLMainActivity_1;
+	LinearLayout lLMainActivity_2;
+	ImageView iVMainActivity_1;
+	
+	EditText eTMainActivity_1;
+	
+	int szer_ekranu;
+	int wys_ekranu;
+	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-
+        Display display = getWindowManager().getDefaultDisplay();
+		Point size = new Point();
+		display.getSize(size);
+		szer_ekranu = size.x;
+		wys_ekranu = size.y;
+		
+        lLMainActivity_1 = (LinearLayout) findViewById(R.id.lLMainActivity_1);
+        lLMainActivity_2 = (LinearLayout) findViewById(R.id.lLMainActivity_2);
+        iVMainActivity_1 = (ImageView) findViewById(R.id.iVMainActivity_1);
+        eTMainActivity_1 = (EditText) findViewById(R.id.eTMainActivity_1);
     }
 
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
